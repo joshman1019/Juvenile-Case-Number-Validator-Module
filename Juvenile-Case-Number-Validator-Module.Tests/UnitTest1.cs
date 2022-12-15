cef3e774019066c90f566cb8517ddfd4a7b718e3
joshman1019 <joshman1019@gmail.com>
2022-12-15 15:31:06 -0500

    Remove Github testing code which shoudl restore the project to a good build!

--- Juvenile-Case-Number-Validator-Module.Tests/UnitTest1.cs
+++ Juvenile-Case-Number-Validator-Module.Tests/UnitTest1.cs
@@ -59,7 +59,6 @@ public class UnitTest1
     [InlineData("", CaseTypes.JudicialWaiver)]
     [InlineData("22 CVD 100", CaseTypes.Delinquent)]
     [InlineData("22 M 100", CaseTypes.Delinquent)]
-    [InlineData("22 JA 100", CaseTypes.AbuseNeglectDependency)] // NOTE: Incorrect push for GitHub actions testing
     public void TestIncorrectFileNumberFormatting(string caseNumber, CaseTypes caseType)
     {
         var result = caseNumber.ConfirmValidJuvenileCaseNumber(caseType);
